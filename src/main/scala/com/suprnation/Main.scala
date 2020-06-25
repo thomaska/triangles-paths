9c3c528e68bd5473497e3c4d9a5cd8578db1dd39
Thomas Kaliakos <thomas.kaliakos@ihsmarkit.com>
2020-06-25 18:53:08 +0300

    Various unsuccessful fixes. Changing approach

--- src/main/scala/com/suprnation/Main.scala
+++ src/main/scala/com/suprnation/Main.scala
@@ -16,20 +16,20 @@ object Main extends IOApp {
   def parseTriangleLevel(prev: List[MutableNode],
                          nodes: List[Int],
                          acc: List[MutableNode]): Either[TriangleError, List[MutableNode]] = {
-    if (nodes.length != prev.length + 1)
-      Left(TriangleParsingError(new Throwable(s"Nodes: $nodes are invalid")))
-    else {
-      (prev, nodes) match {
-        case (Nil, last :: Nil) => Right(acc :+ MutableNode(last))
-        case (parent :: prevLvlTail, left :: right :: newLvlTail) => {
-          val l = MutableNode(left)
-          val r = MutableNode(right)
-          parent.left = l
-          parent.right = r
-          parseTriangleLevel(prevLvlTail, right :: newLvlTail, acc :+ l)
-        }
-        case _ => Left(TriangleParsingError(new Throwable(s"Nodes: $nodes are invalid")))
+    (prev, nodes) match {
+      case (Nil, root :: Nil) => Right(acc :+ MutableNode(root))
+      case (parent :: prevLvlTail, left :: right :: newLvlTail) => {
+        val l = MutableNode(left)
+        val r = MutableNode(right)
+        parent.left = l
+        parent.right = r
+        if (prevLvlTail.nonEmpty)
+          parseTriangleLevel(prevLvlTail, right :: newLvlTail, acc ++ List(l, r))
+        else
+          parseTriangleLevel(prevLvlTail, right :: newLvlTail, acc ++ List(l))
+
       }
+      case _ => Left(TriangleParsingError(new Throwable(s"Error - Nodes: $nodes are invalid")))
     }
   }
 
@@ -37,7 +37,7 @@ object Main extends IOApp {
   def readTriangle(getLine: () => String,
                    prev: List[MutableNode],
                    root: Option[MutableNode]): Either[TriangleError, MutableNode] = {
-    val line = getLine().stripMargin
+    val line = getLine().trim
     if (line == Eof)
       root.toRight(EmptyTriangle)
     else {
@@ -50,7 +50,7 @@ object Main extends IOApp {
         Left(TriangleParsingError(parsed.left.get))
       else {
         val nodes = parsed.right.get
-        val cur = parseTriangleLevel(prev, nodes.toList, List.empty)
+        val cur   = parseTriangleLevel(prev, nodes.toList, List.empty)
 
         if (cur.isLeft)
           Left(cur.left.get)
@@ -63,9 +63,16 @@ object Main extends IOApp {
     }
   }
 
+  def readInput(readLine: () => String, cur: List[String] = List.empty): List[String] = {
+    val line = readLine()
+    if (line == Eof)
+      cur
+    else readInput(readLine, cur :+ line)
+  }
+
   def run(args: List[String]): IO[ExitCode] = {
     IO(println("Application minimum triangle path running"))
-      .map(_ => readTriangle(StdIn.readLine, List.empty, None))
+      .map(_ => readInput(StdIn.readLine))
       .map(_ => ExitCode.Success)
   }
 
--- src/test/scala/com/suprnation/MainTest.scala
+++ src/test/scala/com/suprnation/MainTest.scala
@@ -10,6 +10,21 @@ class MainTest extends FreeSpec with ScalaCheckPropertyChecks with Matchers {
   val arrayGen: Gen[Array[Int]] = Gen.listOf(Gen.choose(0, 99999999)).map(_.toArray)
 
   "Methods tests" - {
+    "readInput should return the correct output" in {
+      val input =
+        """7
+          |6 3
+          |3 8 5
+          |11 2 10 9
+          |EOF
+          |""".stripMargin.split("\n").iterator
+      val actual = Main.readInput(input.next)
+      val expected = List(
+        "7", "6 3", "3 8 5", "11 2 10 9"
+      )
+      actual shouldBe expected
+    }
+
     "parseTriangleLevel should return the correct nodes for the new level if the previous level is the root" in {
       val node     = MutableNode(1)
       val prev     = List(node)
@@ -20,49 +35,72 @@ class MainTest extends FreeSpec with ScalaCheckPropertyChecks with Matchers {
       node shouldBe (MutableNode(1, MutableNode(2), MutableNode(3)))
     }
 
-    val prev     = List(MutableNode(1), MutableNode(2), MutableNode(3))
-    val nodes    = List(4, 5, 6, 7)
-    val expected = Right(List(MutableNode(4), MutableNode(5), MutableNode(6), MutableNode(7)))
-    val actual   = Main.parseTriangleLevel(prev, nodes, List.empty)
-    actual shouldBe expected
-    prev shouldBe List(MutableNode(1, MutableNode(4), MutableNode(5)),
-                       MutableNode(2, MutableNode(5), MutableNode(6)),
-                       MutableNode(3, MutableNode(6), MutableNode(7)))
-  }
+    "parseTriangleLevel should return the correct nodes for the new level" in {
+      val prev     = List(MutableNode(1), MutableNode(2), MutableNode(3))
+      val nodes    = List(4, 5, 6, 7)
+      val expected = Right(List(MutableNode(4), MutableNode(5), MutableNode(6), MutableNode(7)))
+      val actual   = Main.parseTriangleLevel(prev, nodes, List.empty)
+      actual shouldBe expected
+      prev shouldBe List(MutableNode(1, MutableNode(4), MutableNode(5)),
+                         MutableNode(2, MutableNode(5), MutableNode(6)),
+                         MutableNode(3, MutableNode(6), MutableNode(7)))
+    }
 
-  "parseTriangleLevel should return an error if next level is too small" in {
-    val prev   = List(MutableNode(1), MutableNode(2))
-    val nodes  = List(3, 4)
-    val actual = Main.parseTriangleLevel(prev, nodes, List.empty)
-    actual shouldBe a[Left[_, _]]
-  }
+    "parseTriangleLevel should return an error if next level is too small" in {
+      val prev   = List(MutableNode(1), MutableNode(2))
+      val nodes  = List(3, 4)
+      val actual = Main.parseTriangleLevel(prev, nodes, List.empty)
+      actual shouldBe a[Left[_, _]]
+    }
 
-  "parseTriangleLevel should return an error if next level is too big" in {
-    val prev   = List(MutableNode(1), MutableNode(2))
-    val nodes  = List(3, 4, 5, 6)
-    val actual = Main.parseTriangleLevel(prev, nodes, List.empty)
-    actual shouldBe a[Left[_, _]]
-  }
+    "parseTriangleLevel should return an error if next level is too big" in {
+      val prev   = List(MutableNode(1), MutableNode(2))
+      val nodes  = List(3, 4, 5, 6)
+      val actual = Main.parseTriangleLevel(prev, nodes, List.empty)
+      actual shouldBe a[Left[_, _]]
+    }
 
-  "readTriangle should parse a triangle correctly for valid input" in {
-    val input =
-      """7
+    "readTriangle should parse a triangle correctly for valid input" in {
+      val input =
+        """7
           |6 3
           |3 8 5
           |11 2 10 9
           |EOF
           |""".stripMargin.split("\n").iterator
-    val actual = Main.readTriangle(input.next, List.empty, None)
-    val expected = Right(
-      MutableNode(
-        7,
-        MutableNode(6,
-                    MutableNode(3, MutableNode(11, null, null), MutableNode(2, null, null)),
-                    MutableNode(8, MutableNode(2, null, null), MutableNode(10, null, null))),
-        MutableNode(3,
-                    MutableNode(8, MutableNode(2, null, null), MutableNode(10, null, null)),
-                    MutableNode(5, MutableNode(10, null, null), MutableNode(9, null, null)))
-      ))
-    actual shouldBe expected
+      val actual = Main.readTriangle(input.next, List.empty, None)
+      val expected = Right(
+        MutableNode(
+          7,
+          MutableNode(6,
+                      MutableNode(3, MutableNode(11, null, null), MutableNode(2, null, null)),
+                      MutableNode(8, MutableNode(2, null, null), MutableNode(10, null, null))),
+          MutableNode(3,
+                      MutableNode(8, MutableNode(2, null, null), MutableNode(10, null, null)),
+                      MutableNode(5, MutableNode(10, null, null), MutableNode(9, null, null)))
+        ))
+      actual shouldBe expected
+
+      val a = Right(
+        MutableNode(
+          7,
+          MutableNode(6,
+                      MutableNode(3, MutableNode(11, null, null), MutableNode(2, null, null)),
+                      MutableNode(8, MutableNode(2, null, null), MutableNode(10, null, null))),
+          MutableNode(3,
+                      MutableNode(8, MutableNode(2, null, null), MutableNode(10, null, null)),
+                      MutableNode(5, MutableNode(10, null, null), MutableNode(9, null, null)))
+        ))
+      val b = Right(
+        MutableNode(
+          7,
+          MutableNode(6,
+                      MutableNode(3, MutableNode(11, null, null), MutableNode(2, null, null)),
+                      MutableNode(8, null, null)),
+          MutableNode(3,
+                      MutableNode(8, MutableNode(2, null, null), MutableNode(10, null, null)),
+                      MutableNode(5, MutableNode(10, null, null), MutableNode(9, null, null)))
+        ))
+    }
   }
 }
