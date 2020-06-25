2c8cd7877d654345b15cfac713a88909d19b38c4
Thomas Kaliakos <thomas.kaliakos@ihsmarkit.com>
2020-06-25 20:01:27 +0300

    Remove old approach

--- src/main/scala/com/suprnation/Main.scala
+++ src/main/scala/com/suprnation/Main.scala
@@ -13,57 +13,6 @@ import model._
 object Main extends IOApp {
   val Eof = "EOF"
 
-  @tailrec
-  def parseTriangleLevel(prev: List[MutableNode],
-                         nodes: List[Int],
-                         acc: List[MutableNode]): Either[TriangleError, List[MutableNode]] = {
-    (prev, nodes) match {
-      case (Nil, root :: Nil) => Right(acc :+ MutableNode(root))
-      case (parent :: prevLvlTail, left :: right :: newLvlTail) => {
-        val l = MutableNode(left)
-        val r = MutableNode(right)
-        parent.left = l
-        parent.right = r
-        if (prevLvlTail.nonEmpty)
-          parseTriangleLevel(prevLvlTail, right :: newLvlTail, acc ++ List(l, r))
-        else
-          parseTriangleLevel(prevLvlTail, right :: newLvlTail, acc ++ List(l))
-
-      }
-      case _ => Left(TriangleParsingError(new Throwable(s"Error - Nodes: $nodes are invalid")))
-    }
-  }
-
-  @tailrec
-  def readTriangle(getLine: () => String,
-                   prev: List[MutableNode],
-                   root: Option[MutableNode]): Either[TriangleError, MutableNode] = {
-    val line = getLine().trim
-    if (line == Eof)
-      root.toRight(EmptyTriangle)
-    else {
-      val parsed = Try {
-        line.split("\\s+").map(_.toInt)
-      }.toEither
-
-      // using `fold` in Either is not tail recursive
-      if (parsed.isLeft)
-        Left(TriangleParsingError(parsed.left.get))
-      else {
-        val nodes = parsed.right.get
-        val cur   = parseTriangleLevel(prev, nodes.toList, List.empty)
-
-        if (cur.isLeft)
-          Left(cur.left.get)
-        else if (cur.isRight && root.isEmpty && cur.right.get.length == 1)
-          readTriangle(getLine, cur.right.get, Some(cur.right.get.head))
-        else {
-          readTriangle(getLine, cur.right.get, root)
-        }
-      }
-    }
-  }
-
   def readInput(readLine: () => String, cur: List[String] = List.empty): List[String] = {
     val line = readLine()
     if (line == Eof)
@@ -87,10 +36,15 @@ object Main extends IOApp {
     })
   }
 
+  def calculateMaxPath(input: List[List[Int]]): List[Int] = {
+    ???
+  }
+
   def run(args: List[String]): IO[ExitCode] = {
     IO(println("Application minimum triangle path running"))
       .map(_ => readInput(StdIn.readLine))
       .map(validateInput)
+      .map(_.map(calculateMaxPath))
       .map(_ => ExitCode.Success)
   }
 
--- src/main/scala/com/suprnation/model.scala
+++ src/main/scala/com/suprnation/model.scala
@@ -16,6 +16,7 @@ package object model {
   case object EmptyTriangle                        extends TriangleError
   case class InvalidListSizeError(list: List[Int]) extends TriangleError
   case class TriangleParsingError(ex: Throwable)   extends TriangleError
+
   implicit class TriangleParsingErrorOps[A](t: Try[A]) {
     def toTriangleError(): Either[TriangleParsingError, A] = {
       t.toEither.left.map(TriangleParsingError)
